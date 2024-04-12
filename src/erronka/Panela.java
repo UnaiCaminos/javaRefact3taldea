f1dd242a84cb3d4da8d53386d887a6bce729df40
Beñat Portillo <ben_por_ech@goierrieskola.org>
2024-04-12 10:46:50 +0200

    Botoiari izena aldatu klase hoiek pertsona klaseko extend direlako

--- src/erronka/Panela.java
+++ src/erronka/Panela.java
@@ -29,8 +29,8 @@ public class Panela extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnBezeroa = new JButton("Langilea");
-		btnBezeroa.addActionListener(new ActionListener() {
+		JButton btnPertsona = new JButton("Langilea");
+		btnPertsona.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				    String postua = "Jatetxeko superbisatzailea";
 			        String kontsulta ="SELECT * FROM erronkadb.langilea WHERE izena='"+user+"'";
@@ -56,8 +56,8 @@ public class Panela extends JFrame {
 					
 					}
         });
-		btnBezeroa.setBounds(0, 0, 264, 191);
-		contentPane.add(btnBezeroa);
+		btnPertsona.setBounds(0, 0, 264, 191);
+		contentPane.add(btnPertsona);
 		
 		btnProduktua = new JButton("Produktua");
 		btnProduktua.addActionListener(new ActionListener() {
