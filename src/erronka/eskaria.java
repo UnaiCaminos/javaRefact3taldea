6c04aad7ed7801d58b56b059b5dd0ddf7d798c7d
Beñat Portillo <ben_por_ech@goierrieskola.org>
2024-04-22 14:58:21 +0200

    eskaeran katalogoa ikusteko aukera eman

--- src/erronka/eskaria.java
+++ src/erronka/eskaria.java
@@ -5,12 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class eskaria extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 	
 	public eskaria() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -19,7 +28,52 @@ public class eskaria extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(208, 10, 814, 695);
+		contentPane.add(table);
+		
+		JButton btnNewButton = new JButton("Katalogoa");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				konexioa kon=new konexioa();
+				Connection conexion = kon.getConnection();
+				String sql = "SELECT * FROM Langileak";
+				Statement st;
+				ResultSet rs;
+				
+				DefaultTableModel model =new DefaultTableModel();
+				
+				model.addColumn("1");
+				model.addColumn("2");
+				model.addColumn("3");
+				model.addColumn("4");
+				
+				table.setModel(model);
+				String[] array = new String[4];
+				try {
+					st=conexion.createStatement();
+					rs = st.executeQuery(sql);
+					while (rs.next()) {
+						array[0]=rs.getString(1);
+						array[1]=rs.getString(2);
+						array[2]=rs.getString(3);
+						array[3]=rs.getString(4);
+						array[4]=rs.getString(5);
+						model.addRow(array);
+						
+					}
+					
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setBounds(30, 21, 85, 21);
+		contentPane.add(btnNewButton);
 	}
-
 }
 
